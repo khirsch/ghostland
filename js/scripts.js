506dcc125190d0009d2eb46fa7d53f20da21d674
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-07-30 12:27:27 -0700

    adjust modal behavior

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
   $('a.thumbnail').click(function(event) {
     $(this).modal({
       fadeDuration: 1000,
-      fadeDelay: 0.50
+      fadeDelay: 0.25
     });
     return false;
   });
